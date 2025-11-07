8613f8b543a679fc5230330ccf62c369374115ef
Jinghui Geng <jinghuigeng@eduroam-169-233-155-178.ucsc.edu>
2025-11-06 21:10:05 -0800

    terraform formatting

--- infra/main.tf
+++ infra/main.tf
@@ -1,54 +1,54 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
 
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
 }
 
 # Security group to allow HTTP in, all traffic out 
-resource "aws_security_group" "web_sg" {  # what is security group? 
-    name        = "terraform_game_web_sg"
-    description = "Allow HTTP inboud and all outboud"   
+resource "aws_security_group" "web_sg" { # what is security group? 
+  name        = "terraform_game_web_sg"
+  description = "Allow HTTP inboud and all outboud"
 
-    ingress {
-        description = "HTTP from anywhere"
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        description = "all outbound" 
-        from_port   = 0
-        to_port     = 0 
-        protocol   = "-1" 
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    description = "all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_key_pair" "default" {
-    key_name   = "terraform-game-key"
-    public_key = file("~/.ssh/id_rsa.pub")
+  key_name   = "terraform-game-key"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 
 # EC2 instance that runs the game container 
 resource "aws_instance" "web" {
-    ami           = var.ami_id 
-    instance_type = var.instance_type 
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
-    # attach security group (default VPC)
-    vpc_security_group_ids = [aws_security_group.web_sg.id]
-    key_name               = aws_key_pair.default.key_name
+  # attach security group (default VPC)
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name               = aws_key_pair.default.key_name
 
-    user_data = <<-EOF
+  user_data = <<-EOF
         #!/bin/bash
         set -euxo pipefail
 
@@ -67,9 +67,9 @@ resource "aws_instance" "web" {
         docker run -d -p 80:8080 ${var.container_image}
     EOF
 
-    tags = {
-        Name = terraform-aws-game-demo
-    }
+  tags = {
+    Name = terraform-aws-game-demo
+  }
 }
 
 
