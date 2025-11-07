20133b6c8effc4abce6bbf6c77548ec82b065334
Jinghui Geng <jinghuigeng@eduroam-169-233-155-178.ucsc.edu>
2025-11-06 20:25:08 -0800

    fix terraform formatting

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,22 +1,22 @@
 variable "aws_region" {
-    description = "AWS region to deploy into"
-    type        = string     # why do you need a type here? 
-    default     = "us-east-2"
+  description = "AWS region to deploy into"
+  type        = string # why do you need a type here? 
+  default     = "us-east-2"
 }
 
 variable "instance_type" {
-    description = "EC2 instance type"
-    type        = string 
-    default     = "t3.micro"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
 }
 
 variable "ami_id" {
-    description = "Ubuntu AMI ID for the chosen region"
-    type        = string 
-    default     = "ami-0c55b159cbfafe1f0"
+  description = "Ubuntu AMI ID for the chosen region"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0"
 }
 
 variable "container_image" {
-    description = "Docker image (surygeng/terraform-aws-game:lastest)"
-    type        = string
+  description = "Docker image (surygeng/terraform-aws-game:v0.1)"
+  type        = string
 }
